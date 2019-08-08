11ea528117da631795c417f33fe8a6eb43b33487
JUN <soipon0@gmail.com>
2019-08-08 13:08:49 +0900

    ミドルウェアの作成

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/hello', 'HelloController@index')->name('hello');
-Route::get('/hello/other', 'HelloController@other');
-Route::get('/hello/{id}', 'HelloController@index')->where('id', '[0-9]+');
+Route::middleware([HelloMiddleware::class])->group(function () {
+    Route::get('/hello', 'HelloController@index');
+    Route::get('/hello/other', 'HelloController@other');
+});
