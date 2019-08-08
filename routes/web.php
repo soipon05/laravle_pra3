d98359ade4a275bbea751c34439488b82ebd0520
JUN <soipon0@gmail.com>
2019-08-08 12:49:18 +0900

    エラー画面の変更

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', 'HelloController@index')->name('hello');
-
+// Route::get('/hello', 'HelloController@index')->name('hello');
 Route::get('/hello/other', 'HelloController@other');
+Route::get('/hello/{id}', 'HelloController@index')->where('id', '[0-9]+');
